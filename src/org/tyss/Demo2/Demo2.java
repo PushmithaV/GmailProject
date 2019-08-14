bbfc5f8de659feccf5b469211c2df4dceb43ce61
PushmithaV <pushmitha.venugopal@gmail.com>
2019-08-14 12:23:56 +0530

    committed

---
+++ src/org/tyss/Demo2/Demo2.java
@@ -0,0 +1,17 @@
+package org.tyss.Demo2;
+
+import org.tyss.Demo.Demo;
+
+public class Demo2 extends Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Demo d= new Demo();
+		int j = d.demo(20);
+		System.out.println(j);
+	
+		
+		System.out.println(d.i);
+	}
+
+}
